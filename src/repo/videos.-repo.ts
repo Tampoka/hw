a898ff8e5119e8ae23c5147a3059246581befb93
Olga-dot-art <116544878+Olga-dot-art@users.noreply.github.com>
2023-03-05 08:42:55 +0900

    fix

--- src/repo/videos.-repo.ts
+++ src/repo/videos.-repo.ts
@@ -6,14 +6,14 @@ export type VideoType = {
     "minAgeRestriction": number | null
     "createdAt": string
     "publicationDate": string
-    "availableResolutions": string[]
+    "availableResolutions": Array<string>
 }
 export type VideoUpdateType = {
     "title": string
     "author": string
     "minAgeRestriction": number | null
     "publicationDate": string
-    "availableResolutions": string[]
+    "availableResolutions": Array<string>
     "canBeDownloaded": boolean | null
 }
 export const Resolutions = [
@@ -55,7 +55,7 @@ export const videosRepo = {
     findVideo(id: number) {
         return videosData!.find(el => el.id === id)
     },
-    createVideo(title: string, author: string, resolution: string[]) {
+    createVideo(title: string, author: string, resolution: Array<string>) {
         const newVideo: VideoType = {
             id: +(new Date()),
             title,
