89fdabf3acb40c5629ef29ff0adfee6311d37e74
Olga-dot-art <116544878+Olga-dot-art@users.noreply.github.com>
2023-03-04 22:06:41 +0900

    Fix.

--- src/routes/videos-router.ts
+++ src/routes/videos-router.ts
@@ -43,7 +43,7 @@ videosRouter.post('/', (req: Request, res: Response) => {
     const errors: ErrorsType = {
         'errorsMessages': []
     }
-    if (!req.body.title || req.body.title.trim().length > 40) {
+    if (!req.body.title || typeof req.body.title !== 'string' || req.body.title.trim().length > 40) {
         errors.errorsMessages.push({message: 'Title is required', field: 'title'})
     }
     if (!req.body.author || typeof req.body.author !== 'string' || req.body.author.trim().length > 40) {
