5d57e3a821fa4cb4e0a2bf96fddbf6b837bcb22a
Olga-dot-art <116544878+Olga-dot-art@users.noreply.github.com>
2023-03-05 17:49:12 +0900

    fix

--- src/repo/videos.-repo.ts
+++ src/repo/videos.-repo.ts
@@ -62,8 +62,8 @@ export const videosRepo = {
             author,
             canBeDownloaded: false,
             minAgeRestriction: null,
-            createdAt: new Date(new Date().setDate(new Date().getDate() + 1)).toISOString(),
-            publicationDate: new Date(new Date().setDate(new Date().getDate() + 1)).toISOString(),
+            createdAt: new Date(new Date().setDate(new Date().getDate() + 2)).toISOString(),
+            publicationDate: new Date(new Date().setDate(new Date().getDate() + 2)).toISOString(),
             availableResolutions: resolution||["P144"]
         }
         videosData.push(newVideo)
