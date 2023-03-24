1721dbddbca533f3a85f6560f6cd25d806174313
Olga-dot-art <116544878+Olga-dot-art@users.noreply.github.com>
2023-03-24 23:57:20 +0900

    Add posts repo.

---
+++ src/api_v2/repo/posts-repo.ts
@@ -0,0 +1,83 @@
+import {blogsRepo} from './blogs-repo';
+
+export type PostViewModel = {
+    id: string
+    "blogId": string
+    "blogName": string
+    "title": string
+    "shortDescription": string
+    "content": string
+}
+export type PostInputModel = {
+    "title": string
+    "blogId": string
+    "shortDescription": string
+    "content": string
+}
+
+let postsData: PostViewModel[] = [{
+    id: '1',
+    title: 'Pizza',
+    shortDescription: 'Cooking western cuisine',
+    content: 'tra ta ta',
+    blogId: '1',
+    blogName: 'Cooking',
+},
+    {
+        id: '2',
+        title: 'Favorite books',
+        shortDescription: 'Science fiction is my addiction',
+        content: 'bla bla',
+        blogId: '2',
+        blogName: 'Karate',
+    },
+]
+
+export const postsRepo = {
+    findPosts() {
+        return postsData
+    },
+    findPost(id: string) {
+        return postsData!.find(el => el.id === id)
+    },
+    createPost(title: string, shortDescription: string, content: string, blogId: string) {
+        const blogName = blogsRepo.findBlog(blogId)?.name
+        if (blogName) {
+            const newPost: PostViewModel = {
+                id: String(new Date()),
+                title,
+                shortDescription,
+                content,
+                blogId,
+                blogName
+            }
+            postsData.push(newPost)
+            return newPost
+        } else {
+            return false
+        }
+    },
+    updatePost(id: string, valuesToUpdate: PostInputModel) {
+        const post = postsData!.find(el => el.id === id)
+        if (post) {
+            post.title = valuesToUpdate.title || post.title
+            post.shortDescription = valuesToUpdate.shortDescription || post.shortDescription
+            post.content = valuesToUpdate.content || post.content
+            return post
+        } else {
+            return false
+        }
+    },
+    deletePost(id: string) {
+        let index = postsData!.findIndex(el => el.id === id)
+        if (index > -1) {
+            postsData!.splice(index, 1)
+            return true
+        } else {
+            return false
+        }
+    },
+    deleteAll() {
+        postsData = []
+    }
+}
