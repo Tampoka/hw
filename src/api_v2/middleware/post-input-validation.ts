7f544e579433512bcb2d9277eb1cf0ddef4d98ab
Olga-dot-art <116544878+Olga-dot-art@users.noreply.github.com>
2023-03-24 23:57:34 +0900

    Add posts validation.

---
+++ src/api_v2/middleware/post-input-validation.ts
@@ -0,0 +1,42 @@
+import {Response, Request, NextFunction} from 'express';
+import {body, ValidationError, validationResult} from 'express-validator';
+
+const errorFormatter = ({msg}: ValidationError) => {
+    return msg;
+};
+export const titleValidation = body('title').isString().trim().isLength({
+    min: 1,
+    max: 30
+}).withMessage({message: 'Post title should be from 1 to 30 symbols', field: 'title'});
+
+export const shortDescriptionValidation = body('shortDescription').isString().trim().isLength({
+    min: 1,
+    max: 100
+}).withMessage({
+    message: 'Post shortDescription should be from 1 to 100 symbols',
+    field: 'description'
+});
+
+export const contentValidation = body('content').isString().trim().isLength({
+    min: 1,
+    max: 1000
+}).withMessage({
+    message: 'Post content should be from 1 to 1000 symbols',
+    field: 'description'
+});
+
+export const blogIdValidation = body('content').isString().trim().isLength({
+    min: 1,
+}).withMessage({
+    message: 'Post blogId should be from 1 symbol',
+    field: 'description'
+});
+
+export const postInputValidationMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req).formatWith(errorFormatter);
+    if (!errors.isEmpty()) {
+        res.status(400).json({errorMessages: errors.array()})
+    } else {
+        next()
+    }
+}
