db8240d0ba9f90d653db4ef97a3a2b1330f73204
Olga-dot-art <116544878+Olga-dot-art@users.noreply.github.com>
2023-03-24 23:10:55 +0900

    Add validator.

--- src/settings.ts
+++ src/settings.ts
@@ -1,12 +1,14 @@
 import express, {Request, Response} from 'express';
 import {videosRouter} from './api_1v/routes/videos-router';
 import {videosRepo} from './api_1v/repo/videos.-repo';
+import {blogsRepo} from './api_v2/repo/blogs-repo';
+import {blogsRouter} from './api_v2/routes/blogs-router';
 
 export const app = express()
 app.use(express.json());
 
 app.use('/api/v1/videos', videosRouter)
-// app.use('/api/v2/blogs', blogsRouter)
+app.use('/api/v2/blogs', blogsRouter)
 // app.use('/api/v2/posts', postRouter)
 // app.get('/', (req: Request, res: Response) => {
 //     res.send('Hello back-end HomeWorks in it-incubator!!!')
@@ -14,7 +16,7 @@ app.use('/api/v1/videos', videosRouter)
 
 app.delete('/testing/all-data', async (req: Request, res: Response) => {
     await videosRepo.deleteAll()
-    // await blogsRepo.deleteAll()
+    await blogsRepo.deleteAll()
     // await postsRepo.deleteAll()
     res.sendStatus(204)
 })
