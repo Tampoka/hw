c648547cb19e27a011f0847ee437073ff6fe8d26
Olga-dot-art <116544878+Olga-dot-art@users.noreply.github.com>
2023-03-24 23:09:15 +0900

    Add blogs router.

---
+++ src/api_v2/routes/blogs-router.ts
@@ -0,0 +1,58 @@
+import {Request, Response, Router} from 'express';
+import {CodeResponsesEnum} from '../../enums';
+import {blogsRepo} from '../repo/blogs-repo';
+import {
+    blogInputValidationMiddleware,
+    descriptionValidation,
+    nameValidation,
+    websiteUrlValidation
+} from '../middleware/blog-input-validation';
+import {authMiddleware} from '../middleware/isAuth';
+
+export const blogsRouter = Router({})
+
+blogsRouter.get('/', (req: Request, res: Response) => {
+    const result = blogsRepo.findBlogs()
+    res.status(CodeResponsesEnum.OK_200).send(result)
+})
+blogsRouter.get('/:id', (req: Request, res: Response) => {
+    if (!req.params.id) {
+        res.status(CodeResponsesEnum.Not_found_404)
+    }
+    const result = blogsRepo.findBlog(req.params.id)
+    if (result) {
+        res.status(CodeResponsesEnum.OK_200).send(result)
+    } else {
+        res.sendStatus(CodeResponsesEnum.Not_found_404)
+    }
+})
+
+blogsRouter.delete('/:id', authMiddleware, (req: Request, res: Response) => {
+    if (!req.params.id) {
+        res.status(CodeResponsesEnum.Not_found_404)
+    }
+    let result = blogsRepo.deleteBlog(req.params.id);
+    if (result) {
+        res.sendStatus(CodeResponsesEnum.No_content_204)
+    } else {
+        res.sendStatus(CodeResponsesEnum.Not_found_404)
+    }
+})
+blogsRouter.post('/', authMiddleware, nameValidation, descriptionValidation, websiteUrlValidation, blogInputValidationMiddleware, (req: Request, res: Response) => {
+
+    const result = blogsRepo.createBlog(req.body.name, req.body.description, req.body.websiteUrl)
+    res.status(CodeResponsesEnum.Created_201).send(result)
+})
+blogsRouter.put('/:id', authMiddleware, nameValidation, descriptionValidation, websiteUrlValidation, blogInputValidationMiddleware, (req: Request, res: Response) => {
+    if (!req.params.id) {
+        res.status(CodeResponsesEnum.Not_found_404)
+    }
+    const valuesToUpdate = req.body
+
+    const result = blogsRepo.updateBlog(req.params.id, valuesToUpdate)
+    if (result) {
+        res.sendStatus(CodeResponsesEnum.No_content_204)
+    } else {
+        res.sendStatus(CodeResponsesEnum.Not_found_404)
+    }
+})
