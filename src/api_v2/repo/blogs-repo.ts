d7bde5bae78240715f5192cb0d6803118ba7cbec
Olga-dot-art <116544878+Olga-dot-art@users.noreply.github.com>
2023-03-24 23:09:05 +0900

    Add blogs repo.

---
+++ src/api_v2/repo/blogs-repo.ts
@@ -0,0 +1,70 @@
+export type BlogViewModel = {
+    "id": string
+    "name": string
+    "description": string
+    "websiteUrl": string
+}
+export type BlogInputModel = {
+    "name": string
+    "description": string
+    "websiteUrl": string
+}
+
+let blogsData: BlogViewModel[] = [{
+    id: '1',
+    name: 'Cooking',
+    description: 'Cooking western cuisine',
+    websiteUrl: 'https://dimychblog.com',
+
+},
+    {
+        id: '2',
+        name: 'Karate',
+        description: 'Everything about martial arts',
+        websiteUrl: 'https://karate.com',
+
+    },
+]
+
+export const blogsRepo = {
+    findBlogs() {
+        return blogsData
+    },
+    findBlog(id: string) {
+        return blogsData!.find(el => el.id === id)
+    },
+    createBlog(name: string, description: string, websiteUrl: string) {
+        const newBlog: BlogViewModel = {
+            id: String(new Date()),
+            name,
+            description,
+            websiteUrl
+        }
+        blogsData.push(newBlog)
+        return newBlog
+    },
+    updateBlog(id: string, valuesToUpdate: BlogInputModel) {
+        const blog = blogsData!.find(el => el.id === id)
+        if (blog) {
+            blog.name = valuesToUpdate.name || blog.name
+            blog.description = valuesToUpdate.description || blog.description
+            blog.websiteUrl = blog.websiteUrl || blog.websiteUrl
+
+            return blog
+        } else {
+            return false
+        }
+    },
+    deleteBlog(id: string) {
+        let index = blogsData!.findIndex(el => el.id === id)
+        if (index > -1) {
+            blogsData!.splice(index, 1)
+            return true
+        } else {
+            return false
+        }
+    },
+    deleteAll() {
+        blogsData = []
+    }
+}
