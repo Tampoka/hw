7704e05869c2244d86925a8fc105ca3ad295fbb4
Olga-dot-art <116544878+Olga-dot-art@users.noreply.github.com>
2023-03-25 00:05:58 +0900

    Add posts router.

---
+++ src/api_v2/routes/posts-router.ts
@@ -0,0 +1,59 @@
+import {Request, Response, Router} from 'express';
+import {CodeResponsesEnum} from '../../enums';
+import {authMiddleware} from '../middleware/isAuth';
+import {postsRepo} from '../repo/posts-repo';
+import {
+    blogIdValidation,
+    contentValidation,
+    postInputValidationMiddleware,
+    shortDescriptionValidation,
+    titleValidation
+} from '../middleware/post-input-validation';
+
+export const postsRouter = Router({})
+
+postsRouter.get('/', (req: Request, res: Response) => {
+    const result = postsRepo.findPosts()
+    res.status(CodeResponsesEnum.OK_200).send(result)
+})
+postsRouter.get('/:id', (req: Request, res: Response) => {
+    if (!req.params.id) {
+        res.status(CodeResponsesEnum.Not_found_404)
+    }
+    const result = postsRepo.findPost(req.params.id)
+    if (result) {
+        res.status(CodeResponsesEnum.OK_200).send(result)
+    } else {
+        res.sendStatus(CodeResponsesEnum.Not_found_404)
+    }
+})
+
+postsRouter.delete('/:id', authMiddleware, (req: Request, res: Response) => {
+    if (!req.params.id) {
+        res.status(CodeResponsesEnum.Not_found_404)
+    }
+    let result = postsRepo.deletePost(req.params.id);
+    if (result) {
+        res.sendStatus(CodeResponsesEnum.No_content_204)
+    } else {
+        res.sendStatus(CodeResponsesEnum.Not_found_404)
+    }
+})
+postsRouter.post('/', authMiddleware, titleValidation, shortDescriptionValidation, contentValidation, blogIdValidation, postInputValidationMiddleware, (req: Request, res: Response) => {
+
+    const result = postsRepo.createPost(req.body.title, req.body.shortDescription, req.body.content, req.body.blogId)
+    res.status(CodeResponsesEnum.Created_201).send(result)
+})
+postsRouter.put('/:id', authMiddleware, titleValidation, shortDescriptionValidation, contentValidation, blogIdValidation, postInputValidationMiddleware, (req: Request, res: Response) => {
+    if (!req.params.id) {
+        res.status(CodeResponsesEnum.Not_found_404)
+    }
+    const valuesToUpdate = req.body
+
+    const result = postsRepo.updatePost(req.params.id, valuesToUpdate)
+    if (result) {
+        res.sendStatus(CodeResponsesEnum.No_content_204)
+    } else {
+        res.sendStatus(CodeResponsesEnum.Not_found_404)
+    }
+})
--- src/settings.ts
+++ src/settings.ts
@@ -3,13 +3,15 @@ import {videosRouter} from './api_1v/routes/videos-router';
 import {videosRepo} from './api_1v/repo/videos.-repo';
 import {blogsRepo} from './api_v2/repo/blogs-repo';
 import {blogsRouter} from './api_v2/routes/blogs-router';
+import {postsRouter} from './api_v2/routes/posts-router';
+import {postsRepo} from './api_v2/repo/posts-repo';
 
 export const app = express()
 app.use(express.json());
 
 app.use('/api/v1/videos', videosRouter)
 app.use('/api/v2/blogs', blogsRouter)
-// app.use('/api/v2/posts', postRouter)
+app.use('/api/v2/posts', postsRouter)
 // app.get('/', (req: Request, res: Response) => {
 //     res.send('Hello back-end HomeWorks in it-incubator!!!')
 // })
@@ -17,6 +19,6 @@ app.use('/api/v2/blogs', blogsRouter)
 app.delete('/testing/all-data', async (req: Request, res: Response) => {
     await videosRepo.deleteAll()
     await blogsRepo.deleteAll()
-    // await postsRepo.deleteAll()
+    await postsRepo.deleteAll()
     res.sendStatus(204)
 })
