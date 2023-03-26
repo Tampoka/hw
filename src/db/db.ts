61ebb62c040904b1ef154c188dccd27fc7c7fa17
Olga-dot-art <116544878+Olga-dot-art@users.noreply.github.com>
2023-03-26 23:01:46 +0900

    Add db.

---
+++ src/db/db.ts
@@ -0,0 +1,38 @@
+import dotenv from 'dotenv'
+import {MongoClient} from 'mongodb';
+
+dotenv.config()
+
+export type BlogViewModel = {
+    "id": string
+    "name": string
+    "description": string
+    "websiteUrl": string
+}
+export type PostViewModel = {
+    id: string
+    "blogId": string
+    "blogName": string
+    "title": string
+    "shortDescription": string
+    "content": string
+}
+const mongoURI = process.env.MONGO_URL || 'mongodb://localhost:27017'
+console.log(process.env.MONGO_URL)
+
+const client = new MongoClient(mongoURI)
+const db=client.db('backend-dev')
+export const blogsCollection=db.collection<BlogViewModel>('blogs')
+export const postsCollection=db.collection<PostViewModel>('posts')
+
+
+export const runDb = async () => {
+    try {
+        await client.connect()
+        // await client.db('backend-dev').command({ping: 1})
+        console.log('Connected to db!')
+    } catch (e) {
+        console.log('Can\'t connect to db!')
+        await client.close()
+    }
+}
