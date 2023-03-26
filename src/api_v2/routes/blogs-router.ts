34ca00aac7dd6fde5392b337c413bd8f29d296e6
Olga-dot-art <116544878+Olga-dot-art@users.noreply.github.com>
2023-03-26 23:02:13 +0900

    Fix.

--- src/api_v2/routes/blogs-router.ts
+++ src/api_v2/routes/blogs-router.ts
@@ -12,12 +12,12 @@ import {postsRepo} from '../repo/posts-repo';
 
 export const blogsRouter = Router({})
 
-blogsRouter.get('/', (req: Request, res: Response) => {
-    const result = blogsRepo.findBlogs()
+blogsRouter.get('/',  async (req: Request, res: Response) => {
+    const result = await blogsRepo.findBlogs()
     res.status(CodeResponsesEnum.OK_200).send(result)
 })
-blogsRouter.get('/:id', (req: Request, res: Response) => {
-    const result = blogsRepo.findBlog(req.params.id)
+blogsRouter.get('/:id', async (req: Request, res: Response) => {
+    const result = await blogsRepo.findBlog(req.params.id)
     if (result) {
         res.status(CodeResponsesEnum.OK_200).send(result)
     } else {
@@ -25,11 +25,11 @@ blogsRouter.get('/:id', (req: Request, res: Response) => {
     }
 })
 
-blogsRouter.delete('/:id', authMiddleware, (req: Request, res: Response) => {
+blogsRouter.delete('/:id', authMiddleware, async (req: Request, res: Response) => {
     // if (!req.params.id) {
     //     res.status(CodeResponsesEnum.Not_found_404)
     // }
-    let result = blogsRepo.deleteBlog(req.params.id);
+    let result = await blogsRepo.deleteBlog(req.params.id);
     if (result) {
         postsRepo.deleteBlogAllPosts(req.params.id)
         res.sendStatus(CodeResponsesEnum.No_content_204)
@@ -37,17 +37,17 @@ blogsRouter.delete('/:id', authMiddleware, (req: Request, res: Response) => {
         res.sendStatus(CodeResponsesEnum.Not_found_404)
     }
 })
-blogsRouter.post('/', authMiddleware, nameValidation, descriptionValidation, websiteUrlValidation, blogInputValidationMiddleware, (req: Request, res: Response) => {
-    const result = blogsRepo.createBlog(req.body.name, req.body.description, req.body.websiteUrl)
+blogsRouter.post('/', authMiddleware, nameValidation, descriptionValidation, websiteUrlValidation, blogInputValidationMiddleware, async (req: Request, res: Response) => {
+    const result = await blogsRepo.createBlog(req.body.name, req.body.description, req.body.websiteUrl)
     res.status(CodeResponsesEnum.Created_201).send(result)
 })
-blogsRouter.put('/:id', authMiddleware, nameValidation, descriptionValidation, websiteUrlValidation, blogInputValidationMiddleware, (req: Request, res: Response) => {
+blogsRouter.put('/:id', authMiddleware, nameValidation, descriptionValidation, websiteUrlValidation, blogInputValidationMiddleware, async (req: Request, res: Response) => {
     // if (!req.params.id) {
     //     res.status(CodeResponsesEnum.Not_found_404)
     // }
     const valuesToUpdate = req.body
 
-    const result = blogsRepo.updateBlog(req.params.id, valuesToUpdate)
+    const result = await blogsRepo.updateBlog(req.params.id, valuesToUpdate)
     if (result) {
         res.sendStatus(CodeResponsesEnum.No_content_204)
     } else {
---
+++ src/api_v3/middleware/isAuth.ts
@@ -0,0 +1,25 @@
+import {NextFunction, Request, Response} from 'express';
+
+export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const auth = {login: 'admin', password: 'qwerty'} // change this
+
+    // parse login and password from headers
+    const basicAuth = (req.headers.authorization || '').split(' ')[0]
+    if (basicAuth !== 'Basic') {
+        //need explicit return
+        return res.status(401).send('Authentication required.')
+    }
+    const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+
+    const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':')
+
+    // Verify login and password are set and correct
+    if (login && password && login === auth.login && password === auth.password) {
+        // Access granted...
+        return next()
+    } else {
+        // Access denied...
+        // res.set('WWW-Authenticate', 'Basic realm="401"') // change this
+        res.status(401).send('Authentication required.') // custom message
+    }
+}
