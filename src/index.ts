2a1d085bb72e0882ad934d8cd8e90cdbd3b9022e
Olga-dot-art <116544878+Olga-dot-art@users.noreply.github.com>
2023-03-26 23:03:18 +0900

    Connect mongo.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import {app} from './settings'
+import {runDb} from './db/db';
 
 const port = process.env.PORT || 3015
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+const startApp = async () => {
+    await runDb()
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`)
+    })
+}
+
+startApp()
--- src/settings.ts
+++ src/settings.ts
@@ -1,23 +1,23 @@
 import express, {Request, Response} from 'express';
-import {videosRouter} from './api_1v/routes/videos-router';
-import {videosRepo} from './api_1v/repo/videos.-repo';
-import {blogsRepo} from './api_v2/repo/blogs-repo';
-import {blogsRouter} from './api_v2/routes/blogs-router';
-import {postsRouter} from './api_v2/routes/posts-router';
-import {postsRepo} from './api_v2/repo/posts-repo';
+// import {videosRouter} from './api_1v/routes/videos-router';
+import {blogsRepo} from './api_v3/repo/blogs-repo';
+import {blogsRouter} from './api_v3/routes/blogs-router';
+import {postsRouter} from './api_v3/routes/posts-router';
+import {postsRepo} from './api_v3/repo/posts-repo';
 
 export const app = express()
 app.use(express.json());
 
-app.use('/api/v1/videos', videosRouter)
-app.use('/api/v2/blogs', blogsRouter)
-app.use('/api/v2/posts', postsRouter)
+// app.use('/api/v1/videos', videosRouter)
+app.use('/api/v3/blogs', blogsRouter)
+app.use('/api/v3/posts', postsRouter)
 // app.get('/', (req: Request, res: Response) => {
 //     res.send('Hello back-end HomeWorks in it-incubator!!!')
 // })
 
-app.delete('/api/v2/testing/all-data', (req: Request, res: Response) => {
-    blogsRepo.deleteAll()
-    postsRepo.deleteAll()
+app.delete('/api/v3/testing/all-data', async (req: Request, res: Response) => {
+    await blogsRepo.deleteAll()
+    await postsRepo.deleteAll()
     res.sendStatus(204)
 })
+
